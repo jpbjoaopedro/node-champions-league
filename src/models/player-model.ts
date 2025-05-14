371bcfd1ef20eafc78336b3d9fe08d7e1a5701f3
Joao Pedro Bandeira <jpbandeiradossantos@gmail.com>
2025-05-14 16:34:09 -0300

    feat: enhance PlayerModel and update player data in repository

--- src/models/player-model.ts
+++ src/models/player-model.ts
@@ -1,4 +1,16 @@
 export interface PlayerModel {
     id: number;
     name: string;
+    club: string
+    nationality: string;
+    position: string;
+    age: number;
+    statistics: {
+        Overall: number;
+        Attack: number;
+        Defense: number;
+        Passing: number;
+        Dribbling: number;
+        Physical: number;
+    };
 }
--- src/repositories/players-repository.ts
+++ src/repositories/players-repository.ts
@@ -1,27 +1,97 @@
 import { PlayerModel } from "../models/player-model";
 
 const database: PlayerModel[] = [
-    {id: 1, name: "Messi"},
-    {id: 2, name: "Cristiano Ronaldo"},
-    {id: 3, name: "Neymar"},
-    {id: 4, name: "Mbappe"},
-    {id: 5, name: "Kevin De Bruyne"},
-    {id: 6, name: "Robert Lewandowski"},
-    {id: 7, name: "Karim Benzema"},
-    {id: 8, name: "Luka Modric"},
-    {id: 10, name: "Virgil van Dijk"},
-    {id: 11, name: "Sadio Mane"},
-    {id: 12, name: "Mohamed Salah"},
+    {
+        id: 1, 
+        name: "Messi",
+        club: "Inter Miami",
+        nationality: "Argentina",
+        age: 36,
+        position: "Forward",
+        statistics: {
+            Overall: 85,
+            Attack: 90,
+            Defense: 80,
+            Passing: 88,
+            Dribbling: 95,
+            Physical: 75,
+        },
+    },
+    {
+        id: 2,
+        name: "Cristiano Ronaldo",
+        club: "Al Nassr",
+        nationality: "Portugal",
+        age: 38,
+        position: "Forward",
+        statistics: {
+            Overall: 90,
+            Attack: 95,
+            Defense: 85,
+            Passing: 92,
+            Dribbling: 97,
+            Physical: 80,
+        },
+    },
+    {
+        id: 3,
+        name: "Neymar",
+        club: "Al Hilal",
+        nationality: "Brazil",
+        age: 31,
+        position: "Forward",
+        statistics: {
+            Overall: 88,
+            Attack: 92,
+            Defense: 78,
+            Passing: 90,
+            Dribbling: 95,
+            Physical: 70,
+        },
+    },
+    {
+        id: 4,
+        name: "Kylian Mbappé",
+        club: "Paris Saint-Germain",
+        nationality: "France",
+        age: 24,
+        position: "Forward",
+        statistics: {
+            Overall: 89,
+            Attack: 95,
+            Defense: 78,
+            Passing: 90,
+            Dribbling: 95,
+            Physical: 75,
+        },
+    },
+    {
+        id: 5,
+        name: "Kevin De Bruyne",
+        club: "Manchester City",
+        nationality: "Belgium",
+        age: 32,
+        position: "Midfielder",
+        statistics: {
+            Overall: 91,
+            Attack: 85,
+            Defense: 80,
+            Passing: 95,
+            Dribbling: 90,
+            Physical: 78,
+        },
+    },
+
 ]
 
 export const findAllPlayers = async (): Promise<PlayerModel[]> => {
     return database;
 };
 
-export const findPlayerById = async(
+export const findPlayerById = async (
     id: number
 ): Promise<PlayerModel | undefined> => {
     const player = database.find((d) => d.id === id);
-    
+
     return player;
 }
