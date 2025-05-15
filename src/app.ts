11f6bde4cb49441ee92f415f2335144e404a4c4c
Joao Pedro Bandeira <jpbandeiradossantos@gmail.com>
2025-05-15 11:13:05 -0300

    feat: add CORS support to the application and update package dependencies

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 import express from "express";
 import router from "./routes"
+import cors from "cors";
 
 function createApp() {
     const app = express();
 
+    const corsOptions = {
+        origin: '*',
+        methods: ['GET', 'POST', 'DELETE', 'PATCH']
+    };
+
+    app.use(cors(corsOptions));
     app.use(express.json());
     app.use("/api", router)
 
